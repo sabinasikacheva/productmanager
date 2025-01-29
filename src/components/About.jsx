c88ad7fb3de9b10dfe994d33151d68444cb82fd2
Sabina <sabina.sikacheva@yandex.ru>
2025-01-29 12:52:32 +0400

    «test»

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,8 @@ function About() {
     <section id="about" className="about">
       <h2>About Me</h2>
       <p>I'm Pavel Sikachev
-      Seasoned Director of Product Management with over seven years of international experience. When not advising companies or speaking at conferences about technology, I love surfing, tennis and snowboarding and exploring the world while travelling.
+      Seasoned Director of Product Management with over seven years of international experience in B2C and B2B software development for mobile and web applications. Expert in transforming strategic visions into reality, having spearheaded the localization and international scaling of a SaaS product at Jivochat, a competitor to Intercom, leading to a revenue growth of 180%. Successfully built the top HealthTech ecosystem in Europe at Kinship (a division of Mars Inc.). Adept at leading cross-functional teams encompassing product management, product design, engineering, research, and data across both startup and corporate environments.
+Key areas of expertise include Subscription Models, Marketplaces, Mobile Apps, Direct-to-Consumer (D2C), Business-to-Business (B2B), Ecosystem Development, E-Commerce, HealthTech, and AI.
 I also enjoy mentoring young technologists looking to start careers in the tech.</p>
       <h2>Resume</h2>
       <p>You can download my resume by clicking the button below:</p>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Hero.css';
-import pavel from './images/Pavel.jpg'; // Импорт изображения
+import pavel from './images/Pavel_BW.webp'; // Импорт изображения
 
 function Hero() {
   return (
     <section className="hero">
       <div className="hero-content">
         <div className="hero-text">
-          <h1>Hello, I'm Pavel Sikachev</h1>
-          <p>Seasoned Director of Product Management with over seven years of international experience. Based in Yerevan, Armenia.</p>
+          <h1>Pavel Sikachev • Product Leader</h1>
+          <p>I'm happy to share my experience with you. I have expertise in building an International career, leading AI products, Transferring from CPO to CEO and back, Managing ambiguity, and setting goals when nothing is clear. Based in Yerevan, Armenia.</p>
           <a href="#work" className="btn">View My Work</a>
         </div>
         <div className="hero-image">
